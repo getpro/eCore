2117bf8d10ad39a12ff93a28f421d6be324380e4
getpro <flowerdo@126.com>
2016-08-29 23:15:13 +0800

    first

---
+++ app/src/main/java/com/yq/ecore/widget/AlertView.java
@@ -0,0 +1,15 @@
+package com.yq.ecore.widget;
+
+import android.view.View;
+
+/**
+ * Created by YuQi on 2016/8/29.
+ */
+public class AlertView {
+    View decorView;//根视图
+    View contentContainer;//弹出视图
+
+    public AlertView(String title,String content){
+
+    }
+}
